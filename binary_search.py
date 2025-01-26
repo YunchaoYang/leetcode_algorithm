a4438ca77675a25df7a0476957f9c46af4b833cb
Yunchao Yang <yunch.young@gmail.com>
2025-01-26 10:09:07 -0500

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -21,3 +21,17 @@ def upper_bound(arr, first, last, value):
             first = mid + 1
     # loop jump when first == last
     return first
+
+# upper bound could be better done by: 
+# here the lower_bound and upper_bound differs only at if condition
+def upper_bound(arr, first, last, value):
+# 适用于区间为空、答案不存在、有重复元素、搜索开/闭的上/下界等情况
+    # search [first, last) 
+    while first < last: # 搜索区间[first, last)不为空
+        mid = first + (last - first) // 2
+        if arr[mid] <= value:
+            first = mid + 1
+        else: 
+            last = mid
+    # loop jump when first == last
+    return first
